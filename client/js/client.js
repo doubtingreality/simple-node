be8006ef2b3414868dc3037968a3ab15e7d5a406
Murtada al Mousawy <murtada.al.mousawy@gmail.com>
2015-11-30 05:39:23 +0100

    Users and positions
    Added lots of functionality
    - Users are stored internally in server and client object
    - Mouse events are emitted and broadcasted to each client
    - Multiple users are able to connect
    - Users time-out if they don't emit "keepalive" for over 10 seconds

--- client/js/client.js
+++ client/js/client.js
@@ -1,64 +1,262 @@
 (function() {
+var socket
 try {
-	var socket = io.connect('http://127.0.0.1:8080');
+	var socket = io.connect('http://127.0.0.1:8080', {forceNew: true});
 } catch (e) {
 	// Warn user
 	alert(e);
 }
 
+var current_user = {
+	user_name: (Math.round(Math.random()*89999)+10000).toString(),
+};
+
 if (socket !== undefined) {
-	// Listen for output
-	socket.on('broadcast', parseBroadcast);
+
 
 	var users = {};
 
-	function parseBroadcast(data) {
-		var cursors = document.body.children;
-		for (var i = 0, len = cursors.length; i < len; i++) {
-			if (typeof cursors[i] == "object") {
-				document.body.removeChild(cursors[i]);
+	sendJoin();
+
+	window.addEventListener("beforeunload", function (e) {
+		sendQuit();
+	});
+
+	function sendQuit(e) {
+		console.log("Quitting");
+		socket.emit('quit', {
+			user_name: current_user.user_name
+		});
+		socket.disconnect();
+	}
+
+	function sendJoin(e) {
+		console.log("Joining as: ", current_user.user_name);
+		socket.emit('join', {
+			user_name: current_user.user_name
+		});
+	}
+
+	// Listen for output
+	socket.on('broadcast', parseBroadcast);
+	socket.on('users', parseUsers);
+	socket.on('quit', parseQuit);
+
+	socket.on('mouseclick', parseMouseclick);
+	//socket.on('mousedown', parseMousedown);
+	//socket.on('mouseup', parseMouseup);
+
+	function parseMouseclick(data) {
+		var user_name = data.user_name,
+		    user_pos  = data.user_position;
+
+		console.log("Clicked:", user_name);
+
+		if (user_name !== current_user.user_name) {
+			if (!(users[user_name] === undefined)) {
+
+				users[user_name].user_position = user_pos;
+				add_block(user_pos);
 			}
 		}
+	}
 
+	function parseMousedown(data) {
+		var user_name = data.user_name,
+		    user_pos  = data.user_position;
 
-		for (var i = 0, len = data.length; i < len; i++) {
-			var pos = data[i].user_position;
+		if (!(users[user_name] === undefined)) {
+			users[user_name].user_position = user_pos;
+			users[user_name].mousedown = true;
+		}
+	}
 
-			var cursor = document.createElement("div");
-			cursor.classList.add("cursor");
-			cursor.style.top = pos.y+"px";
-			cursor.style.left = pos.x+"px";
+	function parseMouseup(data) {
+		var user_name = data.user_name,
+		    user_pos  = data.user_position;
 
-			document.body.appendChild(cursor);
+		if (!(users[user_name] === undefined)) {
+			users[user_name].user_position = user_pos;
+			users[user_name].mousedown = false;
+		}
+	}
+
+	function parseQuit(data) {
+		if (!(users[data] === undefined)) {
+			console.log("User quit:", data);
+			document.body.removeChild(users[data].cursor);
+			delete users[data];
+		}
+		console.log("Users after quit:", users);
+	}
+
+	function parseUsers(data) {
+		console.log(data);
+		if (data) {
+			for (var i = 0, len = data.length; i < data.length; i++) {
+				var user_name = data[i].user_name;
+				if (users[user_name] === undefined) {
+					users[user_name] = {};
+					users[user_name].user_position = {x: 0, y: 0};
+					var cursor = document.createElement("div");
+					cursor.id = "cursor-"+user_name;
+					cursor.classList.add("cursor");
+
+					users[user_name].cursor = cursor;
+
+					console.log("New user:", user_name);
+
+					if (user_name !== current_user.user_name) {
+						document.body.appendChild(cursor);
+					}
+				}
+
+			}
+			console.log("Joined users:", users);
+		}
+	}
+
+	function parseBroadcast(data) {
+		if (data) {
+			for (var i = 0, len = data.length; i < data.length; i++) {
+				var user_name = data[i].user_name;
+				var pos = data[i].user_position;
+
+				if (users[user_name]) {
+					users[user_name].user_position = pos;
+				}
+			}
+
+			render();
+		}
+	}
+
+	function render() {
+		var user_key;
+		for (user_key in users) {
+			if ((user_key !== current_user.user_name) && (users[user_key].user_position)) {
+				var cursor = users[user_key].cursor;
+				var pos = users[user_key].user_position;
+				cursor.style.transform = "translate("+pos.x+"px, "+pos.y+"px)";
+			}
 		}
 	}
 
 	// Send position
-	document.addEventListener("mousemove", sendPosition);
+	setInterval(function() {
+		sendPosition();
+	}, 50);
 
-	function sendPosition(e) {
-		socket.emit('input', {
-			user_name: "Murtada",
+	var amount = 0;
+	function pushbuttonClicked() {
+		amount++;
+		document.getElementById("pushbutton-text").textContent = "Clicked "+amount+" times";
+	}
+
+	document.getElementById("pushbutton").addEventListener("click", pushbuttonClicked);
+	document.addEventListener("mousemove", getPosition);
+	//document.addEventListener("click", sendMouseclick);
+	document.addEventListener("mousedown", sendMousedown);
+	document.addEventListener("mouseup", sendMouseup)
+
+	var pos_x = 0;
+	var pos_y = 0;
+
+	function sendMouseclick(e) {
+		socket.emit('mouseclick', {
+			user_name: current_user.user_name,
 			user_position: {
 				x: e.clientX,
 				y: e.clientY
 			}
 		});
 	}
+
+	function sendMousedown(e) {
+		if (!current_user.mousedown) {
+			socket.emit('mouseclick', {
+				user_name: current_user.user_name,
+				user_position: {
+					x: e.clientX,
+					y: e.clientY
+				}
+			});
+			add_block({x: e.clientX, y: e.clientY})
+			current_user.mousedown = true;
+		}
+		pauseEvent(e);
+	}
+
+	function sendMouseup(e) {
+		socket.emit('mouseup', {
+			user_name: current_user.user_name,
+			user_position: {
+				x: e.clientX,
+				y: e.clientY
+			}
+		});
+		current_user.mousedown = false;
+	}
+
+	function getPosition(e) {
+		pos_x = e.clientX;
+		pos_y = e.clientY;
+	}
+
+	function sendPosition() {
+		socket.emit('input', {
+			user_name: current_user.user_name,
+			user_position: {
+				x: pos_x,
+				y: pos_y
+			}
+		});
+	}
+
+	// Keepalive
+	setInterval(function() {
+		keepAlive();
+	}, 1000);
+
+	function keepAlive(e) {
+		socket.emit('keepalive', true);
+	}
+}
+
+function pauseEvent(e){
+    if(e.stopPropagation) e.stopPropagation();
+    if(e.preventDefault) e.preventDefault();
+    e.cancelBubble=true;
+    e.returnValue=false;
+    return false;
+}
+
+var blocks_layer = document.createElement("div");
+blocks_layer.id = "blocks-layer";
+document.body.appendChild(blocks_layer);
+
+function add_block(pos) {
+	var block = document.createElement("div");
+	block.classList.add("block");
+
+	pos.x -= 10;
+	pos.y -= 10;
+	block.style.transform = "translate("+pos.x+"px, "+pos.y+"px)";
+
+	blocks_layer.appendChild(block);
+}
+
+function click_in_window(x,y) {
+    var ev = document.createEvent("MouseEvent");
+    var el = document.elementFromPoint(x,y);
+    ev.initMouseEvent(
+        "click",
+        true /* bubble */, true /* cancelable */,
+        window, null,
+        x, y, 0, 0, /* coordinates */
+        false, false, false, false, /* modifier keys */
+        0 /*left*/, null
+    );
+    el.dispatchEvent(ev);
 }
 })();
-
-function array_flip( trans )
-{
-    var key, tmp_ar = {};
-
-    for ( key in trans )
-    {
-        if ( trans.hasOwnProperty( key ) )
-        {
-            tmp_ar[trans[key]] = key;
-        }
-    }
-
-    return tmp_ar;
-}
--- server/server.js
+++ server/server.js
@@ -1,51 +1,152 @@
-var mongo  = require('mongodb').MongoClient;
-    client = require('socket.io').listen(8080).sockets;
+var mongo   = require('mongodb').MongoClient;
+    client  = require('socket.io').listen(8080).sockets,
+    clients = {};
 
 // Connect to database
 mongo.connect('mongodb://127.0.0.1/simple-node', function(error, db) {
 	if (error) throw error;
 
+	var collection_users     = db.collection('users'),
+		collection_positions = db.collection('positions');
+
+	var data_all_users,
+	    data_all_positions;
+
+	var users = {};
+
+
+	collection_users.remove({});
+	collection_positions.remove({});
+
+	var get_all_users = function(callback) {
+		return collection_users.find().limit(10).toArray(function(error, response) {
+			data_all_users = response;
+			console.log(data_all_users);
+			callback();
+		});
+	};
+
+	var get_all_positions = function(callback) {
+		return collection_positions.find().sort({_id: -1}).limit(10).toArray(function(error, response) {
+			data_all_positions = response;
+			callback();
+		});
+	};
+
+	/*setInterval(function() {
+		data_all_positions = get_all_positions();
+	}, 50);*/
+
 	// User is connected
 	client.on('connection', function(socket) {
+		clients[socket.id] = socket;
 
-		var collection_users     = db.collection('users'),
-		    collection_positions = db.collection('positions');
-
-		var broadcast = function(data) {
-			socket.emit('broadcast', data);
-		};
-
-		var getDatabaseData = function() {
-			var data_user = collection_users.find().sort({_id: -1}).limit(10).toArray(function(error, response){
-				broadcast(response);
-			});
-		};
-
-		setInterval(function() {
-			getDatabaseData();
-		}, 100);
-
-		// Connect user
-		socket.on('connect', function(data) {
-			var user_name     = data.user_name;
-
-			collection_users.insert({"user_name": user_name});
-
+		socket.on('disconnect', function() {
+			delete clients[socket.id];
 		});
 
-		// Wait for input
+		var current_user = { timeout: 0 };
+
+		var broadcast_users = function(key_string) {
+			var user_array = Object.keys(users).map(function(k) { return users[k] });
+			client.emit(key_string, user_array);
+		};
+
+		/*setInterval(function() {
+			broadcast_positions();
+		}, 50);*/
+
+		// Client input
 		socket.on('input', function(data) {
+			users[data.user_name] = data;
+			broadcast_users('broadcast');
+		});
+
+		// Client mouseclick
+		socket.on('mouseclick', function(data) {
 			var user_name     = data.user_name,
 			    user_position = data.user_position;
 
-			collection_users.insert(
-				{"user_name": user_name, "user_position": user_position},
-				function(error, result) {
-					if (error) throw error;
-				}
-			);
+			client.emit('mouseclick', {"user_name": user_name, "user_position": user_position});
 		});
 
+		// Client mousedown
+		socket.on('mousedown', function(data) {
+			var user_name     = data.user_name,
+			    user_position = data.user_position;
+
+			client.emit('mousedown', {"user_name": user_name, "user_position": user_position});
+			pauseEvent(e);
+		});
+
+		// Client mouseup
+		socket.on('mouseup', function(data) {
+			var user_name     = data.user_name,
+			    user_position = data.user_position;
+
+			client.emit('mouseup', {"user_name": user_name, "user_position": user_position});
+		});
+
+		// Client join
+		socket.on('join', function(data) {
+			var user_name = data.user_name;
+			current_user.user_name = user_name;
+
+
+			if (users[user_name] === undefined) {
+				users[user_name] = {
+					user_name: data.user_name
+				};
+				console.log("user joined", user_name);
+			}
+
+			console.log(users);
+			broadcast_users('users');
+
+		});
+
+		// Client quit
+		socket.on('quit', function(data) {
+			var user_name = data.user_name;
+
+			if (!(users[user_name] === undefined)) {
+				console.log("User quit:", user_name);
+				delete users[user_name];
+			}
+			console.log(users);
+
+			client.emit('quit', user_name);
+			clearTimeout(timer_timeout);
+			socket.disconnect();
+		});
+
+		// Client keepalive
+		socket.on('keepalive', function(data) {
+			if (data) {
+				current_user.timeout = 0;
+			}
+		});
+
+		var timer_timeout = setInterval(clientTimeout, 1000);
+
+		function clientTimeout() {
+			if (current_user.timeout > 10 && !(current_user.user_name === undefined)) {
+				collection_users.remove(
+					{"user_name": current_user.user_name},
+					function(error, result) {
+						if (error) throw error;
+					}
+				);
+
+				console.log("Client timed out:", current_user.user_name);
+
+				client.emit('quit', current_user.user_name);
+				clearTimeout(timer_timeout);
+				socket.disconnect();
+			} else {
+				current_user.timeout++;
+			}
+		}
 	});
 });
 
